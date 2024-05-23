edb7a1bbf15dab052846a266c3259ee9734b13b4
Regan Tunstall <1rtunsta@hbhs.school.nz>
2024-05-23 14:02:56 +1200

    Add project files.

---
+++ Platform Game/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Platform_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.player = new System.Windows.Forms.PictureBox();
+            this.screen = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.screen.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Black;
+            this.player.Location = new System.Drawing.Point(519, 290);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(30, 31);
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // screen
+            // 
+            this.screen.Controls.Add(this.player);
+            this.screen.Location = new System.Drawing.Point(1, 0);
+            this.screen.Name = "screen";
+            this.screen.Size = new System.Drawing.Size(1019, 551);
+            this.screen.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1016, 548);
+            this.Controls.Add(this.screen);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.screen.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Panel screen;
+    }
+}
+
---
+++ Platform Game/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Platform_Game
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        bool left, right, jump;
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Move left and right
+            if (e.KeyCode == Keys.Right) { right = true; }
+            if (e.KeyCode == Keys.Left) { left = true; }
+            if (jump != true)
+            {
+                if (e.KeyCode == Keys.Space)
+                {
+                    jump = true;
+                    force = g;
+                }
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            //Stop moving left and right
+            if (e.KeyCode == Keys.Right) { right = false; }
+            if (e.KeyCode == Keys.Left) { left = false; }
+        }
+
+        int force = 17;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (right == true) { player.Left += 5; }//5 pixels to the right
+            if (left == true) { player.Left -= 5; }//5 pixels to the left
+            //Falling
+            if (jump == true)
+            {
+                player.Top -= force;
+                force -= 1;
+
+            }
+            if (player.Top + player.Height >= screen.Height)
+            {
+                player.Top = screen.Height - player.Height; //Stop Falling
+            }
+
+        }
+    }
+}
